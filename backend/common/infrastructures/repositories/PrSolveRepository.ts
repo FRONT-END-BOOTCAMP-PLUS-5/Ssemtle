16aab56f1748d64412d5e940bf2cae7a9a9d65a9
myuceller <myuceller@github.com>
2025-08-08 15:53:29 +0900

    fix/build err 수정

--- backend/common/infrastructures/repositories/PrSolveRepository.ts
+++ backend/common/infrastructures/repositories/PrSolveRepository.ts
@@ -4,6 +4,7 @@ import prisma from '@/libs/prisma';
 import { SolveRepository } from '@/backend/common/domains/repositories/SolveRepository';
 
 export class PrSolveRepository implements SolveRepository {
+
   async create(solve: Omit<Solve, 'id' | 'createdAt'>): Promise<Solve> {
     const { unit, user, ...data } = solve; // ❗ unit, user 제외
 
@@ -13,4 +14,13 @@ export class PrSolveRepository implements SolveRepository {
 
     return saved;
   }
+
+  async findHelpTextById(solveId: number): Promise<{ helpText: string } | null> {
+    const res = await prisma.solve.findUnique({
+      where: { id: solveId },
+      select: { helpText: true },
+    });
+    return res; // { helpText } | null
+  }
+  
 }

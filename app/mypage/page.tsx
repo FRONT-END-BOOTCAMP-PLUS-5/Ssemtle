c7d386e944a372aed16dcde74a54e651ac375da8
myuceller <myuceller@github.com>
2025-08-13 10:53:50 +0900

    fix/mypage api호출하는법 고민중

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -3,14 +3,34 @@
 import CalendarComponent from './components/CalenderComponent';
 import React from 'react';
 import RadarChartComponent from './components/RadarChartComponent';
+import { useGets } from '@/hooks/useGets';
+import { GetStudentUnitPerformanceResponseDTO } from '@/backend/analysis/dtos/GetStudentUnitPerformanceDTO';
+import { CreateUserResponseDto } from '@/backend/auth/dtos/UserDto';
 
 export default function MyPage() {
+  const { data: analysisData } = useGets<GetStudentUnitPerformanceResponseDTO>(
+    ['mypage'],
+
+    '/students/unit',
+
+    true
+  );
+
+  const { data: userData } = useGets<CreateUserResponseDto>(
+    ['auth', 'session'],
+    '/auth/session',
+    true
+  );
+
+  console.log('analysisData', analysisData);
+  console.log('userData', userData);
+  console.log('BASE_URL', process.env.NEXT_PUBLIC_BASE_URL);
   return (
     <main className="flex min-h-screen flex-col items-center bg-[rgb(254,247,255)]">
       {/* Title */}
       <div className="w-full text-center">
         <h1 className="text-2xl font-semibold sm:text-3xl">
-          동규의 마이페이지
+          {userData?.user.name}의 마이페이지
         </h1>
       </div>
       <RadarChartComponent />

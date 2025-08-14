358172bb30a58bc9a86b756df9e4ef32e25f6e08
Leem Jung Hoon <xjqhdjq@naver.com>
2025-08-14 11:46:22 +0900

    style: 단원평가 페이지 수정중

--- app/unit/page.tsx
+++ app/unit/page.tsx
@@ -1,7 +1,12 @@
 const UnitPage = () => {
   return (
-    <div>
-      <span className="text-2xl font-bold">단원 평가</span>
+    <div className="flex flex-col p-10">
+      <div className="border-black-100 flex items-center justify-start border-2">
+        <span className="flex justify-self-start text-2xl font-bold">
+          단원 평가
+        </span>
+        <div className="justify flex">test</div>
+      </div>
     </div>
   );
 };
